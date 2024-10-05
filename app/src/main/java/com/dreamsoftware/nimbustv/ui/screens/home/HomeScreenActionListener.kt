09179d92aa91cc7e5d43ca03e720c60c124a7921
Sergio Sánchez <sss4esob@gmail.com>
2024-10-05 14:47:09 +0200

    Add M3U parser library, implement ImportPlayListDialog

--- app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/home/HomeScreenActionListener.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/home/HomeScreenActionListener.kt
@@ -2,4 +2,9 @@ package com.dreamsoftware.nimbustv.ui.screens.home
 
 import com.dreamsoftware.fudge.core.IFudgeTvScreenActionListener
 
-interface HomeScreenActionListener: IFudgeTvScreenActionListener
+interface HomeScreenActionListener: IFudgeTvScreenActionListener {
+
+    fun onImportNewPlaylistClicked()
+    fun onImportNewPlaylistCancelled()
+    fun onNewPlayListUrlUpdated(newValue: String)
+}
--- app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/home/HomeScreenContent.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/home/HomeScreenContent.kt
@@ -2,6 +2,7 @@ package com.dreamsoftware.nimbustv.ui.screens.home
 
 import androidx.compose.runtime.Composable
 import com.dreamsoftware.fudge.component.FudgeTvScreenContent
+import com.dreamsoftware.nimbustv.ui.screens.home.components.importer.ImportPlaylistDialog
 import com.dreamsoftware.nimbustv.ui.screens.home.components.importer.NoPlaylistFound
 
 @Composable
@@ -10,9 +11,16 @@ internal fun HomeScreenContent(
     actionListener: HomeScreenActionListener
 ) {
     with(state) {
-        FudgeTvScreenContent(onErrorAccepted = actionListener::onErrorMessageCleared) {
-            NoPlaylistFound {
-
+        with(actionListener) {
+            FudgeTvScreenContent(onErrorAccepted = ::onErrorMessageCleared) {
+                ImportPlaylistDialog(
+                    isVisible = isImportPlaylistDialogVisible,
+                    playListUrl = newPlayListUrl,
+                    onAcceptClicked = {},
+                    onPlayListUrlUpdated = ::onNewPlayListUrlUpdated,
+                    onCancelClicked = ::onImportNewPlaylistCancelled
+                )
+                NoPlaylistFound(onImportClicked = ::onImportNewPlaylistClicked)
             }
         }
     }
