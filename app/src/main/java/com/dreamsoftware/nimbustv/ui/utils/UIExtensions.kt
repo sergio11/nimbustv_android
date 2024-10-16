c87d80c3b89b6efe4fe5e4d82e1b6ffb74415cd6
Sergio Sánchez <sss4esob@gmail.com>
2024-10-16 21:30:05 +0200

    Implement EPG screen

--- app/src/main/java/com/dreamsoftware/nimbustv/ui/utils/UIExtensions.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/ui/utils/UIExtensions.kt
@@ -2,6 +2,8 @@ package com.dreamsoftware.nimbustv.ui.utils
 
 import com.dreamsoftware.nimbustv.R
 import com.dreamsoftware.nimbustv.domain.model.AvatarTypeEnum
+import com.dreamsoftware.nimbustv.domain.model.ProgrammeDataBO
+import java.time.format.DateTimeFormatter
 
 val String.Companion.EMPTY: String
     get() = ""
@@ -10,9 +12,13 @@ val Char.Companion.SPACE: Char
     get() = ' '
 
 fun AvatarTypeEnum.toDrawableResource(): Int =
-    when(this) {
+    when (this) {
         AvatarTypeEnum.BOY -> R.drawable.profile_avatar_boy
         AvatarTypeEnum.GIRL -> R.drawable.profile_avatar_girl
         AvatarTypeEnum.WOMAN -> R.drawable.profile_avatar_woman
         AvatarTypeEnum.MAN -> R.drawable.profile_avatar_man
     }
+
+fun ProgrammeDataBO.toScheduleFormatted() =
+    "${startTime.format(DateTimeFormatter.ofPattern("HH:mm"))} - " +
+            endTime.format(DateTimeFormatter.ofPattern("HH:mm"))
