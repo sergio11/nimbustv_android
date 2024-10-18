1446b4807d04f85060c31c393c915261dbe01b9d
Sergio Sánchez <sss4esob@gmail.com>
2024-10-18 21:15:09 +0200

    Implement favorite screen

--- app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/favorites/FavoritesScreenContent.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/favorites/FavoritesScreenContent.kt
@@ -2,13 +2,13 @@ package com.dreamsoftware.nimbustv.ui.screens.favorites
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.dreamsoftware.fudge.component.FudgeTvFocusRequester
@@ -18,6 +18,9 @@ import com.dreamsoftware.fudge.component.FudgeTvScreenContent
 import com.dreamsoftware.fudge.component.FudgeTvText
 import com.dreamsoftware.fudge.component.FudgeTvTextTypeEnum
 import com.dreamsoftware.nimbustv.R
+import com.dreamsoftware.nimbustv.domain.model.ChannelBO
+import com.dreamsoftware.nimbustv.ui.core.components.ChannelGridItem
+import com.dreamsoftware.nimbustv.ui.core.components.CommonLazyVerticalGrid
 
 @Composable
 internal fun FavoritesScreenContent(
@@ -28,35 +31,72 @@ internal fun FavoritesScreenContent(
         FudgeTvScreenContent(onErrorAccepted = actionListener::onErrorMessageCleared) {
             if (isLoading) {
                 FudgeTvLoadingState(modifier = Modifier.fillMaxSize())
-            } else if(true) {
+            } else if (channels.isEmpty()) {
                 FudgeTvNoContentState(
                     modifier = Modifier.fillMaxSize(),
-                    messageRes = R.string.favorites_not_workout_available
+                    messageRes = R.string.favorites_screen_no_channels_found_text
                 )
-            }else {
-                Column(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    FudgeTvText(
-                        modifier = Modifier.padding(bottom = 8.dp, top = 56.dp, start = 32.dp, end = 32.dp),
-                        type = FudgeTvTextTypeEnum.HEADLINE_MEDIUM,
-                        titleRes = R.string.favorites_screen_title,
-                        textBold = true
-                    )
-                    FudgeTvFocusRequester { focusRequester ->
-                        LazyHorizontalGrid(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 24.dp),
-                            contentPadding = PaddingValues(horizontal = 46.dp),
-                            rows = GridCells.Fixed(2),
-                            horizontalArrangement = Arrangement.spacedBy(24.dp),
-                            verticalArrangement = Arrangement.spacedBy(24.dp)
-                        ) {
-                        }
-                    }
-                }
+            } else {
+                FavoriteChannelsMainContent(
+                    channels = channels,
+                    actionListener = actionListener
+                )
             }
+        }
+    }
+}
+
+@Composable
+private fun FavoriteChannelsMainContent(
+    channels: List<ChannelBO>,
+    actionListener: FavoritesScreenActionListener
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        FavoriteChannelsHeader(actionListener = actionListener)
+        FavoriteChannelsGridContent(
+            channels = channels,
+            onChannelSelected = { }
+        )
+    }
+}
+
+@Composable
+private fun FavoriteChannelsHeader(actionListener: FavoritesScreenActionListener) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp)
+            .padding(end = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        FudgeTvText(
+            type = FudgeTvTextTypeEnum.HEADLINE_MEDIUM,
+            titleRes = R.string.favorites_screen_title,
+            textBold = true
+        )
+    }
+}
+
+@Composable
+private fun FavoriteChannelsGridContent(
+    channels: List<ChannelBO>,
+    onChannelSelected: (ChannelBO) -> Unit
+) {
+    FudgeTvFocusRequester { focusRequester ->
+        CommonLazyVerticalGrid(
+            modifier = Modifier.fillMaxWidth(),
+            state = rememberLazyGridState(),
+            items = channels
+        ) { item ->
+            ChannelGridItem(
+                channel = item,
+                onChannelPressed = onChannelSelected
+            )
         }
     }
 }
--- app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/ui/screens/favorites/FavoritesViewModel.kt
@@ -5,18 +5,25 @@ import com.dreamsoftware.fudge.core.IFudgeTvErrorMapper
 import com.dreamsoftware.fudge.core.SideEffect
 import com.dreamsoftware.fudge.core.UiState
 import com.dreamsoftware.nimbustv.di.FavoritesScreenErrorMapper
+import com.dreamsoftware.nimbustv.domain.model.ChannelBO
+import com.dreamsoftware.nimbustv.domain.usecase.GetFavoriteChannelsByProfileUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoritesViewModel @Inject constructor(
+    private val getFavoriteChannelsByProfileUseCase: GetFavoriteChannelsByProfileUseCase,
     @FavoritesScreenErrorMapper private val errorMapper: IFudgeTvErrorMapper,
 ) : FudgeTvViewModel<FavoritesUiState, FavoritesSideEffects>(), FavoritesScreenActionListener {
 
     override fun onGetDefaultState(): FavoritesUiState = FavoritesUiState()
 
     fun fetchData() {
-
+        executeUseCase(
+            useCase = getFavoriteChannelsByProfileUseCase,
+            onSuccess = ::onGetFavoritesChannelsByProfileCompleted,
+            onMapExceptionToState = ::onMapExceptionToState
+        )
     }
 
     private fun onMapExceptionToState(ex: Exception, uiState: FavoritesUiState) =
@@ -24,11 +31,16 @@ class FavoritesViewModel @Inject constructor(
             isLoading = false,
             errorMessage = errorMapper.mapToMessage(ex)
         )
+
+    private fun onGetFavoritesChannelsByProfileCompleted(channels: List<ChannelBO>) {
+        updateState { it.copy(channels = channels) }
+    }
 }
 
 data class FavoritesUiState(
     override val isLoading: Boolean = false,
-    override val errorMessage: String? = null
+    override val errorMessage: String? = null,
+    val channels: List<ChannelBO> = emptyList()
 ): UiState<FavoritesUiState>(isLoading, errorMessage) {
     override fun copyState(isLoading: Boolean, errorMessage: String?): FavoritesUiState =
         copy(isLoading = isLoading, errorMessage = errorMessage)
