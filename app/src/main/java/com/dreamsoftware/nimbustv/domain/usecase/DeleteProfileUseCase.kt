e088600a808d713a57722b81a4282d91288f8784
Sergio Sánchez <sss4esob@gmail.com>
2024-10-22 21:29:31 +0200

    Implement Sync EPG data worker

--- app/src/main/java/com/dreamsoftware/nimbustv/domain/usecase/DeleteProfileUseCase.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/domain/usecase/DeleteProfileUseCase.kt
@@ -2,13 +2,18 @@ package com.dreamsoftware.nimbustv.domain.usecase
 
 import com.dreamsoftware.fudge.core.FudgeTvUseCaseWithParams
 import com.dreamsoftware.nimbustv.domain.repository.IProfilesRepository
+import com.dreamsoftware.nimbustv.domain.service.IEpgSchedulerService
 
 class DeleteProfileUseCase(
-    private val profilesRepository: IProfilesRepository
+    private val profilesRepository: IProfilesRepository,
+    private val epgSchedulerService: IEpgSchedulerService
 ) : FudgeTvUseCaseWithParams<DeleteProfileUseCase.Params, Boolean>() {
 
-    override suspend fun onExecuted(params: Params): Boolean =
-        profilesRepository.deleteProfile(params.profileId)
+    override suspend fun onExecuted(params: Params): Boolean = with(params) {
+        profilesRepository.deleteProfile(profileId).also {
+            epgSchedulerService.cancelSyncEpgWork(profileId)
+        }
+    }
 
     data class Params(
         val profileId: String
---
+++ app/src/main/java/com/dreamsoftware/nimbustv/framework/epg/scheduler/worker/SyncEpgDataWorker.kt
@@ -0,0 +1,57 @@
+package com.dreamsoftware.nimbustv.framework.epg.scheduler.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.Data
+import androidx.work.WorkerParameters
+import com.dreamsoftware.nimbustv.domain.repository.IEpgRepository
+import com.dreamsoftware.nimbustv.domain.service.IEpgParserService
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+@HiltWorker
+class SyncEpgDataWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val epgRepository: IEpgRepository,
+    private val epgParserService: IEpgParserService
+) : CoroutineWorker(context, workerParams) {
+
+    companion object {
+
+        const val WORK_NAME = "SaveEpgWork"
+        private const val KEY_EPG_URL = "epg_url"
+        private const val KEY_PROFILE_ID = "profile_id"
+
+        @JvmStatic
+        fun buildInputData(epgUrl: String, profileId: String) =
+            Data.Builder()
+                .putString(KEY_EPG_URL, epgUrl)
+                .putString(KEY_PROFILE_ID, profileId)
+                .build()
+    }
+
+    override suspend fun doWork(): Result = try {
+        val url = inputData.getString(KEY_EPG_URL).orEmpty()
+        val profileId = inputData.getString(KEY_PROFILE_ID).orEmpty()
+        if(url.isNotEmpty() && profileId.isNotEmpty()) {
+            Log.d(WORK_NAME, "SaveEpgWorker url: $url, profileId: $profileId")
+            val data = epgParserService.parseEpgData(
+                profileId = profileId,
+                url = url
+            )
+            with(epgRepository) {
+                deleteAllByProfileId(profileId)
+                epgRepository.save(data)
+            }
+            Result.success()
+        } else {
+            Result.failure()
+        }
+    } catch (exception: Exception) {
+        Log.e(WORK_NAME, "An error occurred with trying to sync epg data: ${exception.message}")
+        Result.failure()
+    }
+}
