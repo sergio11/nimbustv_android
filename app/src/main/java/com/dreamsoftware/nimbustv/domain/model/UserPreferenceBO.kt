4eaa2e605aa14b154c0a1600dfcdccdb14822db3
Sergio Sánchez <sss4esob@gmail.com>
2024-10-26 13:34:19 +0200

    Add new preference for configuring EPG style

--- app/src/main/java/com/dreamsoftware/nimbustv/domain/model/UserPreferenceBO.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/domain/model/UserPreferenceBO.kt
@@ -1,5 +1,6 @@
 package com.dreamsoftware.nimbustv.domain.model
 
 data class UserPreferenceBO(
-    val enableSearch: Boolean
+    val enableSearch: Boolean,
+    val epgViewMode: EpgViewModeEnum,
 )
--- app/src/main/java/com/dreamsoftware/nimbustv/domain/usecase/SaveUserPreferencesUseCase.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/domain/usecase/SaveUserPreferencesUseCase.kt
@@ -1,8 +1,10 @@
 package com.dreamsoftware.nimbustv.domain.usecase
 
 import com.dreamsoftware.fudge.core.FudgeTvUseCaseWithParams
+import com.dreamsoftware.nimbustv.domain.model.EpgViewModeEnum
 import com.dreamsoftware.nimbustv.domain.model.UserPreferenceBO
 import com.dreamsoftware.nimbustv.domain.repository.IPreferencesRepository
+import com.dreamsoftware.nimbustv.utils.enumOfOrDefault
 
 class SaveUserPreferencesUseCase(
     private val preferencesRepository: IPreferencesRepository
@@ -14,9 +16,11 @@ class SaveUserPreferencesUseCase(
 
     private fun Params.toUserPreferencesBO() = UserPreferenceBO(
         enableSearch = enableSearch,
+        epgViewMode = enumOfOrDefault({it.value == epgViewMode}, EpgViewModeEnum.NOW_AND_SCHEDULE),
     )
 
     data class Params(
-        val enableSearch: Boolean
+        val enableSearch: Boolean,
+        val epgViewMode: String,
     )
 }
