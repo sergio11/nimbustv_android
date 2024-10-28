e61aa56efc4fb3b6902cf07706c475d52f008aa5
Sergio Sánchez <sss4esob@gmail.com>
2024-10-28 21:54:01 +0100

    User can import several EPGs, preparing repository for doing that

--- app/src/main/java/com/dreamsoftware/nimbustv/data/database/datasource/IProgrammeLocalDataSource.kt
+++
@@ -1,6 +0,0 @@
-package com.dreamsoftware.nimbustv.data.database.datasource
-
-import com.dreamsoftware.nimbustv.data.database.datasource.impl.core.ISupportLocalDataSource
-import com.dreamsoftware.nimbustv.data.database.entity.ProgrammeEntity
-
-interface IProgrammeLocalDataSource: ISupportLocalDataSource<ProgrammeEntity, String>
