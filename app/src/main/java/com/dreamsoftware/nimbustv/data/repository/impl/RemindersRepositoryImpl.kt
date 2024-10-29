3f48235c69986e51212813be0255c6c895fb05ed
Sergio Sánchez <sss4esob@gmail.com>
2024-10-29 21:07:38 +0100

    Implement reminder worker

--- app/src/main/java/com/dreamsoftware/nimbustv/data/repository/impl/RemindersRepositoryImpl.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/data/repository/impl/RemindersRepositoryImpl.kt
@@ -5,6 +5,8 @@ import com.dreamsoftware.nimbustv.data.database.entity.ReminderEntity
 import com.dreamsoftware.nimbustv.data.database.exception.DatabaseException
 import com.dreamsoftware.nimbustv.data.repository.impl.core.SupportRepositoryImpl
 import com.dreamsoftware.nimbustv.domain.exception.CreateReminderException
+import com.dreamsoftware.nimbustv.domain.exception.DeleteReminderByIdException
+import com.dreamsoftware.nimbustv.domain.exception.FetchReminderByIdException
 import com.dreamsoftware.nimbustv.domain.exception.FetchRemindersByProfileException
 import com.dreamsoftware.nimbustv.domain.model.CreateReminderBO
 import com.dreamsoftware.nimbustv.domain.model.ReminderBO
@@ -18,6 +20,17 @@ internal class RemindersRepositoryImpl(
     private val createReminderMapper: IOneSideMapper<CreateReminderBO, ReminderEntity>,
     dispatcher: CoroutineDispatcher
 ): SupportRepositoryImpl(dispatcher), IRemindersRepository {
+
+    @Throws(FetchReminderByIdException::class)
+    override suspend fun findById(id: String): ReminderBO = safeExecute {
+        try {
+            reminderLocalDataSource
+                .findById(id)
+                .let(remindersMapper::mapInToOut)
+        } catch (ex: DatabaseException) {
+            throw FetchReminderByIdException("An error occurred when fetching reminder by id", ex)
+        }
+    }
 
     @Throws(FetchRemindersByProfileException::class)
     override suspend fun findAllByProfileId(profileId: String): List<ReminderBO> = safeExecute {
@@ -39,6 +52,15 @@ internal class RemindersRepositoryImpl(
                 .let(remindersMapper::mapInToOut)
         } catch (ex: DatabaseException) {
             throw FetchRemindersByProfileException("An error occurred when trying to save a new reminder", ex)
+        }
+    }
+
+    @Throws(DeleteReminderByIdException::class)
+    override suspend fun deleteById(id: String): Unit = safeExecute {
+        try {
+            reminderLocalDataSource.delete(id)
+        } catch (ex: DatabaseException) {
+            throw DeleteReminderByIdException("An error occurred when trying to delete reminders", ex)
         }
     }
 }
