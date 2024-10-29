fad3b5122e3d0423b2529f388cb8fc8b543fd693
Sergio Sánchez <sss4esob@gmail.com>
2024-10-29 19:34:25 +0100

    Cancel all sync epg workers when deleting the profile

--- app/src/main/java/com/dreamsoftware/nimbustv/domain/usecase/DeleteProfileUseCase.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/domain/usecase/DeleteProfileUseCase.kt
@@ -1,18 +1,20 @@
 package com.dreamsoftware.nimbustv.domain.usecase
 
 import com.dreamsoftware.fudge.core.FudgeTvUseCaseWithParams
+import com.dreamsoftware.nimbustv.domain.repository.IEpgRepository
 import com.dreamsoftware.nimbustv.domain.repository.IProfilesRepository
 import com.dreamsoftware.nimbustv.domain.service.IEpgSchedulerService
 
 class DeleteProfileUseCase(
+    private val epgRepository: IEpgRepository,
     private val profilesRepository: IProfilesRepository,
     private val epgSchedulerService: IEpgSchedulerService
 ) : FudgeTvUseCaseWithParams<DeleteProfileUseCase.Params, Boolean>() {
 
     override suspend fun onExecuted(params: Params): Boolean = with(params) {
-        profilesRepository.deleteProfile(profileId).also {
-            epgSchedulerService.cancelSyncEpgWork(profileId)
-        }
+        epgRepository.findAllByProfileId(profileId)
+            .forEach { epgSchedulerService.cancelSyncEpgWork(it.id) }
+        profilesRepository.deleteProfile(profileId)
     }
 
     data class Params(
