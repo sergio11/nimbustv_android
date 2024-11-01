147fa68889a051f30ecabf83a771e237e8b7da68
Sergio Sánchez <sss4esob@gmail.com>
2024-11-01 10:30:52 +0100

    Fix EPG last updated

--- app/src/main/java/com/dreamsoftware/nimbustv/data/database/dao/EpgDao.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/data/database/dao/EpgDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Query
 import com.dreamsoftware.nimbustv.data.database.dao.core.SupportDaoImpl
 import com.dreamsoftware.nimbustv.data.database.entity.EpgEntity
+import java.util.Date
 
 @Dao
 abstract class EpgDao: SupportDaoImpl<EpgEntity, String>(EpgEntity::class) {
@@ -14,6 +15,6 @@ abstract class EpgDao: SupportDaoImpl<EpgEntity, String>(EpgEntity::class) {
     @Query(value = "DELETE FROM epg WHERE profile_id = :profileId")
     abstract suspend fun deleteAllByProfileId(profileId: String)
 
-    @Query("UPDATE epg SET last_updated_at = CURRENT_TIMESTAMP WHERE id = :epgId")
-    abstract suspend fun updateLastUpdatedAt(epgId: String)
+    @Query("UPDATE epg SET last_updated_at = :date WHERE id = :epgId")
+    abstract suspend fun updateLastUpdatedAt(epgId: String, date: Date)
 }
