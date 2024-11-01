316e67ecfcf368386a44e70053084dffaa7237c4
Sergio Sánchez <sss4esob@gmail.com>
2024-11-01 12:23:27 +0100

    Implement UI error mappers

--- app/src/main/java/com/dreamsoftware/nimbustv/ui/player/video/VideoPlayerScreenActionListener.kt
+++
@@ -1,9 +0,0 @@
-package com.dreamsoftware.nimbustv.ui.player.video
-
-import com.dreamsoftware.fudge.core.IFudgeTvScreenActionListener
-
-interface VideoPlayerScreenActionListener: IFudgeTvScreenActionListener {
-    fun onAddFavoriteChannelClicked()
-    fun onRemoveChannelFromFavorites()
-    fun onOpenSettingsClicked()
-}
