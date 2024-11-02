504b259b9b927eb9350b74ca49982c0f8c12eaa9
Sergio Sánchez <sss4esob@gmail.com>
2024-11-02 12:09:57 +0100

    Implement several improvements in Player component

--- app/src/main/java/com/dreamsoftware/nimbustv/ui/core/components/CommonPlayerBackground.kt
+++ app/src/main/java/com/dreamsoftware/nimbustv/ui/core/components/CommonPlayerBackground.kt
@@ -5,28 +5,46 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.SentimentDissatisfied
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.compose.runtime.State
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.media3.common.PlaybackException
 import androidx.media3.common.util.UnstableApi
 import androidx.tv.material3.MaterialTheme
 import com.dreamsoftware.fudge.component.FudgeTvLoadingState
+import com.dreamsoftware.fudge.component.FudgeTvText
+import com.dreamsoftware.fudge.component.FudgeTvTextTypeEnum
 import com.dreamsoftware.fudge.utils.handleDPadKeyEvents
+import com.dreamsoftware.nimbustv.R
 import com.dreamsoftware.nimbustv.ui.core.player.PlayerFactory
 import com.dreamsoftware.nimbustv.ui.core.player.SupportPlayer
+import com.dreamsoftware.nimbustv.ui.core.player.state.PlayerState
 import com.dreamsoftware.nimbustv.ui.core.player.state.PlayerStateListener
 import com.dreamsoftware.nimbustv.utils.disableCertificateValidation
 
@@ -37,7 +55,7 @@ import com.dreamsoftware.nimbustv.utils.disableCertificateValidation
  * @param videResource video resource URL.
  * @param videoResourceId Raw resource ID for the video.
  * @param disableCertValidation Flag to disable certificate validation.
- * @param playerStateListener Listener for player state events.
+ * @param onStateChanged Callback for playback state changed events
  * @param onLeft Callback for left D-pad key press.
  * @param onRight Callback for right D-pad key press.
  * @param onEnter Callback for enter key press.
@@ -52,17 +70,40 @@ fun CommonPlayerBackground(
     videoResourceLicenseKey: String? = null,
     disableCertValidation: Boolean = true,
     isOnlyAudio: Boolean = false,
-    playerStateListener: PlayerStateListener? = null,
+    onStateChanged: ((PlayerState) -> Unit)? = null,
     onLeft: (() -> Unit)? = null,
     onRight: (() -> Unit)? = null,
     onEnter: (() -> Unit)? = null,
     content: @Composable BoxScope.(supportPlayer: SupportPlayer) -> Unit = {}
 ) {
+    var playbackError by remember { mutableStateOf(false) }
+    var isPlaybackLoading by remember { mutableStateOf(true) }
+    val playerStateListener = remember {
+        object : PlayerStateListener {
+            override fun onStateChanged(state: PlayerState) {
+                onStateChanged?.invoke(state)
+                if (state is PlayerState.Buffering || state is PlayerState.Complete) {
+                    playbackError = false
+                    isPlaybackLoading = true
+                }
+                if (state is PlayerState.Playing) {
+                    playbackError = false
+                    isPlaybackLoading = false
+                }
+            }
+
+            override fun onErrorOccurred(cause: PlaybackException) {
+                isPlaybackLoading = false
+                playbackError = true
+            }
+        }
+    }
+
     if (disableCertValidation) {
         disableCertificateValidation()
     }
 
-    if(!videResource.isNullOrBlank() || videoResourceId != null) {
+    if (!videResource.isNullOrBlank() || videoResourceId != null) {
         val context = LocalContext.current
         val lifecycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
         val player = remember { PlayerFactory.create(context) }
@@ -76,26 +117,32 @@ fun CommonPlayerBackground(
             playerStateListener = playerStateListener
         )
 
-        if(!isOnlyAudio) {
-            PlayerViewContent(
-                modifier = modifier,
-                player = player,
-                onLeft = onLeft,
-                onRight = onRight,
-                onEnter = onEnter,
-                content = content
-            )
-        } else {
+        if (isOnlyAudio) {
             AudioPlayerViewContent(
                 modifier = modifier,
                 player = player,
                 content = content
             )
+        } else {
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                PlayerViewContent(
+                    modifier = modifier,
+                    player = player,
+                    onLeft = onLeft,
+                    onRight = onRight,
+                    onEnter = onEnter,
+                    content = content
+                )
+                if (isPlaybackLoading) {
+                    PlaybackLoading()
+                }
+                if (playbackError) {
+                    PlaybackError()
+                }
+            }
         }
     } else {
-        FudgeTvLoadingState(modifier = Modifier
-            .background(MaterialTheme.colorScheme.background)
-            .fillMaxSize())
+        PlaybackLoading()
     }
 }
 
@@ -144,16 +191,21 @@ private fun InitializeAndPlayVideo(
                 Lifecycle.Event.ON_PAUSE -> {
                     player.pause()
                 }
+
                 Lifecycle.Event.ON_RESUME -> {
                     playContent()
                 }
+
                 else -> {}
             }
         }
         val lifecycle = lifecycleOwner.value.lifecycle
         lifecycle.addObserver(observer)
         onDispose {
-            player.release()
+            player.apply {
+                playerStateListener?.let(::removePlaybackEvent)
+                release()
+            }
             lifecycle.removeObserver(observer)
         }
     }
@@ -198,14 +250,46 @@ private fun PlayerViewContent(
     content: @Composable BoxScope.(supportPlayer: SupportPlayer) -> Unit = {}
 ) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        AndroidView(factory = { player.getView() }, modifier = Modifier
-            .fillMaxSize()
-            .handleDPadKeyEvents(
-                onEnter = onEnter,
-                onLeft = onLeft,
-                onRight = onRight
-            )
-            .focusable())
+        AndroidView(
+            factory = { player.getView() }, modifier = Modifier
+                .fillMaxSize()
+                .handleDPadKeyEvents(
+                    onEnter = onEnter,
+                    onLeft = onLeft,
+                    onRight = onRight
+                )
+                .focusable()
+        )
         content(player)
+    }
+}
+
+@Composable
+private fun PlaybackLoading() {
+    FudgeTvLoadingState(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+    )
+}
+
+@Composable
+private fun PlaybackError() {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            Icons.Rounded.SentimentDissatisfied,
+            modifier = Modifier.size(64.dp),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        FudgeTvText(
+            titleRes = R.string.channel_playback_error_occurred,
+            type = FudgeTvTextTypeEnum.LABEL_LARGE,
+            textAlign = TextAlign.Center
+        )
     }
 }
